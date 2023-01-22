72dde365870ac3e005737ffcace2c5ea4583e229
Emmany <65581596+Emmanytech@users.noreply.github.com>
2023-01-22 22:42:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+/*addEventListener('DOMContentLoaded', function(){
+    document.querySelector('#switch').onclick = function(){
+       
+        document.querySelector('body').style.color = 'aqua';
+        if(onclick,style.color ='aqua'){
+            document.querySelector('body').style.color = 'red';
+        }*/
+addEventListener('DOMContentLoaded', function(){
+    let bgc = document.querySelector('#switch');
+    bgc.onclick = function(){
+               
+                
+        
+        if(document.querySelector('header'). style.backgroundColor ==="green" ){
+            document.querySelector('header').style.backgroundColor = "rgb(8, 2, 36)";
+           
+        
+        }
+        else{
+            document.querySelector('header'). style.backgroundColor= "green"
+        }
+
+    }    
+      
+            
+                
+            
+         
+})
+    
+ 
